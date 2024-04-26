c7e13bc9da6592179fe9c7a024072eeb71657016
ycyoondev <z20152002@gmail.com>
2024-04-26 17:09:52 +0900

    refact: input 1 초과일때 공통화

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -4,11 +4,8 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int i) {
         List<Integer> factors = new ArrayList<>();
-        if (i == 2) {
-            factors.add(2);
-        }
-        if (i == 3) {
-            factors.add(3);
+        if (i > 1) {
+            factors.add(i);
         }
         return factors;
     }
